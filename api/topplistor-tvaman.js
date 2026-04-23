13a3bba43bb0c8b9e1ccc5e219399f2c26243ebb
matteek89 <matteek89@gmail.com>
2026-04-23 23:47:00 +0200

    Add API handler to return saved data

--- api/topplistor-tvaman.js
+++ api/topplistor-tvaman.js
@@ -1,1 +1,11 @@
+let savedData = {};
 
+export default async function handler(req, res) {
+  try {
+    return res.status(200).json(savedData);
+  } catch (error) {
+    return res.status(500).json({
+      error: error.message
+    });
+  }
+}
